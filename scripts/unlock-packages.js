f8bcc5d6909cf29c6a82deed02bd45fb695008ae
Boris Serdiuk <serdiuk@amazon.com>
2022-10-10 22:32:54 +0200

    add lockfiles for sub packages (#18)

--- scripts/unlock-packages.js
+++ scripts/unlock-packages.js
@@ -6,13 +6,19 @@ const fs = require('fs');
 /**
  * Remove specific @cloudscape-design/* packages where we should always use the latest minor release
  */
-const filename = require.resolve('../package-lock.json');
-const packageLock = require(filename);
+[
+  require.resolve('../package-lock.json'),
+  require.resolve('../packages/converter/package-lock.json'),
+  require.resolve('../packages/core/package-lock.json')
+].forEach(filename => unlock(filename));
 
-Object.keys(packageLock.dependencies).forEach(dependency => {
-  if (dependency.startsWith('@cloudscape-design/')) {
-    delete packageLock.dependencies[dependency];
-  }
-});
-fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + '\n');
-console.log('Removed @cloudscape-design/ dependencies from package-lock file');
+function unlock(filename) {
+  const packageLock = require(filename);
+  Object.keys(packageLock.dependencies).forEach(dependency => {
+    if (dependency.startsWith('@cloudscape-design/')) {
+      delete packageLock.dependencies[dependency];
+    }
+  });
+  fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + '\n');
+  console.log(`Removed @cloudscape-design/ dependencies from ${filename} file`);
+}
