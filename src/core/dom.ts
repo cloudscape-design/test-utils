8563e11af092e9df6d6734c6d6ba0188f252998e
Georgii Lobko <globko@amazon.de>
2024-07-24 11:23:34 +0200

    Revert "chore: export act function (#58)"
    This reverts commit a61d5736284680bf8c0c3f677b5fc89a64d4ff87.

--- src/core/dom.ts
+++ src/core/dom.ts
@@ -5,8 +5,7 @@ import * as React from 'react';
 import { act as reactDomAct } from 'react-dom/test-utils';
 import { IElementWrapper } from './interfaces';
 import { KeyCode, isScopedSelector, substituteScope } from './utils';
-
-export const act = ('act' in React ? React.act : reactDomAct) as typeof reactDomAct;
+const act = ('act' in React ? React.act : reactDomAct) as typeof reactDomAct;
 
 // Original KeyboardEventInit lacks some properties https://github.com/Microsoft/TypeScript/issues/15228
 declare global {
