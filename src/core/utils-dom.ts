a89feb30566d54ebc2827a3689b43277b413f788
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2025-02-20 07:05:45 +0100

    chore: Adds set native value dom util (#81)

--- src/core/utils-dom.ts
+++ src/core/utils-dom.ts
@@ -7,3 +7,25 @@ import * as React from 'react';
 import { act as reactDomAct } from 'react-dom/test-utils';
 
 export const act = ('act' in React ? React.act : reactDomAct) as typeof reactDomAct;
+
+// Original Copyright 2017-Present Kent C. Dodds. Licensed under MIT License.
+// Testing Library code
+
+/* istanbul ignore next @preserve */
+export function setNativeValue(element: Element, value: string): void {
+  const { set: valueSetter } = Object.getOwnPropertyDescriptor(element, 'value') || {};
+  const prototype = Object.getPrototypeOf(element);
+  const { set: prototypeValueSetter } = Object.getOwnPropertyDescriptor(prototype, 'value') || {};
+
+  if (prototypeValueSetter && valueSetter !== prototypeValueSetter) {
+    prototypeValueSetter.call(element, value);
+  } else {
+    if (valueSetter) {
+      valueSetter.call(element, value);
+    } else {
+      throw new Error('The given element does not have a value setter');
+    }
+  }
+}
+
+// End of Testing Library code
