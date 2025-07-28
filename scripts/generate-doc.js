7f4b4187a06a0029cfda0145bbff524ae2394c36
Boris Serdiuk <serdiuk@amazon.com>
2025-07-28 12:09:34 +0200

    chore: migrate to the new documenter version

--- scripts/generate-doc.js
+++ scripts/generate-doc.js
@@ -1,25 +1,11 @@
 #!/usr/bin/env node
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const { documentTestUtils } = require('@cloudscape-design/documenter');
-const fs = require('node:fs');
+const { writeTestUtilsDocumentation } = require('@cloudscape-design/documenter');
 
-const outputPath = './lib/core/test-utils-doc';
-if (!fs.existsSync(outputPath)) {
-  fs.mkdirSync(outputPath, { recursive: true });
-}
-
-const testUtilTypes = ['dom', 'selectors'];
-testUtilTypes.forEach(testUtilType => {
-  const wrapperDefinitions = documentTestUtils(
-    {
-      tsconfig: require.resolve('../tsconfig.docs.json'),
-      includeDeclarations: true,
-      excludeExternals: true,
-    },
-    `./lib/core/**/${testUtilType}.d.ts`,
-  );
-
-  const fileContent = `module.exports = ${JSON.stringify(wrapperDefinitions)}`;
-  fs.writeFileSync(`${outputPath}/${testUtilType}.js`, fileContent);
+writeTestUtilsDocumentation({
+  outDir: './lib/core/test-utils-doc',
+  tsconfigPath: require.resolve('../tsconfig.docs.json'),
+  domUtils: { root: 'src/core/dom.ts', extraExports: ['createWrapper', 'usesDom'] },
+  selectorsUtils: { root: 'src/core/selectors.ts', extraExports: ['createWrapper'] },
 });
